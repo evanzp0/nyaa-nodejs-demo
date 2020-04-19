4096b1fa458e4a7cddd49ef3579379c3f292dcdf
evan <pingplus@sohu.com>
2020-04-19 15:23:32 +0800

    fix: chap02 - first.cpp bug that building with nodejs v13

--- 1. first build/first.cpp
+++ 1. first build/first.cpp
@@ -1,8 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-19 14:44:43
+ * @LastEditTime: 2020-04-19 14:54:31
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /nyaa-nodejs-demo/1. first build/first.cpp
+ */
 #include <node.h>
 
 namespace __first__ {
 
 using v8::FunctionCallbackInfo;
+using v8::NewStringType;
 using v8::Isolate;
 using v8::Local;
 using v8::Object;
@@ -12,7 +21,8 @@ using v8::Value;
 void Method(const FunctionCallbackInfo<Value>& args)
 {
     Isolate* isolate = args.GetIsolate();
-    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "first build"));
+    // args.GetReturnValue().Set(String::NewFromUtf8(isolate, "first build"));
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "first build").ToLocalChecked());
 }
 
 void init(Local<Object> exports)
